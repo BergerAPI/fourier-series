604bcb91f415abcf8069a640186459b59735aa2b
niclas <bergerbtw@gmail.com>
2021-07-18 09:48:03 +0200

    basic sine wave

--- source/sketch.js
+++ source/sketch.js
@@ -1,7 +1,30 @@
+// Height of wave
+let amplitude = 75.0;
+
+// Currenr angle of wave
+let theta = 0.0;
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
+  background(0);
+  noFill();
+  stroke(252, 238, 33);
+  strokeWeight(8);
 
+  theta += 0.01;
+
+  beginShape()
+
+  let z = theta
+  for (let x = 0; x < width + 1; x++) {
+    let y = height / 2 + sin(z + theta) * amplitude
+
+    vertex(x, y);
+    z += TWO_PI / 500
+  }
+
+  endShape()
 }
